8c9a7a4be612f3ac6e3c12ea387b0a3324a7cf4a
melenglobal <75787400+melenglobal@users.noreply.github.com>
2021-03-23 18:02:16 +0300

    loading bar added

--- northwind/src/app/components/product/product.component.ts
+++ northwind/src/app/components/product/product.component.ts
@@ -12,6 +12,7 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class ProductComponent implements OnInit {
   products: Product[] = [];
+  dataLoaded = false;
  
   /* productResponseModel:ProductResponseModel = {}; */
   constructor(private productService:ProductService) {}
@@ -22,8 +23,10 @@ export class ProductComponent implements OnInit {
 
   getProducts()
   {
+    
     this.productService.getProducts().subscribe(response => {
       this.products = response.data;
+      this.dataLoaded =true;
     })
   }
 }
